9ebef029c4d206616b90bc84170320650b72a036
Daniel <daniel.h@develeap.com>
2020-02-23 00:33:12 +0000

    modified import-data.sh

--- db-migrate/docker-files/import-data.sh
+++ db-migrate/docker-files/import-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sqlplus -s SYSTEM/Oradoc_db1@10.12.9.222:1521/ORCLCDBXDB.localdomain << SQL
+sqlplus -s SYSTEM/Oradoc_db1@10.12.9.222:1521/ORCLCDB.localdomain << SQL
 @sql.sql
 exit;
 SQL
